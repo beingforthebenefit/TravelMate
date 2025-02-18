df2e9cf42ce1a572f0ed345da38f6a1e4ea074c1
Gerald Todd <beingforthebenefit@gmail.com>
2025-02-17 16:45:33 -0800

    Add apollo client wrapper

---
+++ packages/frontend/components/ApolloProviderWrapper.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "../lib/apollo-client";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function ApolloProviderWrapper({ children }: Props) {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
