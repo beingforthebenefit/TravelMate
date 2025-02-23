c5ee57d477505e484dc7cf09f7cc97e4f295b194
Gerald Todd <beingforthebenefit@gmail.com>
2025-02-23 14:33:53 -0800

    Better backend jest config

---
+++ packages/backend/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts', 'json'],
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+  },
+  testRegex: '.*\\.(spec|e2e)\\.ts$',
+  collectCoverageFrom: [
+    "**/*.(ts)",
+    "!**/node_modules/**",
+    "!**/dist/**"
+  ],
+};
