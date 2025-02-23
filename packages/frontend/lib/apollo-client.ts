cf8601c168946699745d23618ebeee4b6ac3a3b9
Gerald Todd <beingforthebenefit@gmail.com>
2025-02-23 13:46:46 -0800

    Update apollo client to use cross-fetch

--- packages/frontend/lib/apollo-client.ts
+++ packages/frontend/lib/apollo-client.ts
@@ -1,7 +1,11 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import fetch from 'cross-fetch';
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://backend:3001/graphql',
+  link: new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://backend:3001/graphql',
+    fetch,
+  }),
   cache: new InMemoryCache(),
 });
 
