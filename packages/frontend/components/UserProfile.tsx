57eddbe40507392537c4493cd1a7def4456375de
Gerald Todd <beingforthebenefit@gmail.com>
2025-02-23 14:01:05 -0800

    Add UserProfile component

---
+++ packages/frontend/components/UserProfile.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function UserProfile() {
+    // For now, simply return a placeholder.
+    return (
+        <div className="text-right">
+            <p className="text-gray-800 dark:text-gray-200">Welcome, Test User</p>
+        </div>
+    );
+}
